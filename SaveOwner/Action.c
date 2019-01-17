f422997bd358f40d5f87a77ac3a35a11e2579731
xuping2012 <oupoor@qq.com>
2019-01-17 20:06:09 +0800

    xxx

--- SaveOwner/Action.c
+++ SaveOwner/Action.c
@@ -3,47 +3,44 @@ Action()
 //相册动态-->发布商品-->返回动态
 
 	web_reg_save_param_ex("ParamName=shop_id",
-						"LB=123456\",\"shop_id\":\"",
+						"LB=shop_id\":\"",
 						"RB=\",\"watermark",
 						"NotFound=warning",
 						SEARCH_FILTERS,
 						LAST);
+	lr_think_time(3);
+
 
 	web_custom_request("owner",
-					   "Url=https://{requestUrl}/service/album/get_album_themes_list.jsp?act=owner&shop_id=&search_value=&search_img=&start_date=&end_date=&page_index=1&client_type=ios&token={userToken}&version=2302",
+					   "Url={requestUrl}/service/album/get_album_themes_list.jsp?act=owner&shop_id=&search_value=&search_img=&start_date=&end_date=&page_index=1&client_type=ios&token={userToken}&version=2302",
 					   "Method=POST",
 					   "Mode=HTTP",
 					   LAST);
 
 	lr_start_transaction("相册动态新增商品");
 
-    web_reg_save_param_ex("ParamName=mark_code",
-						  "LB=mark_code\":\"",
-						  "RB=\"},\"errmsg",
-						  "NotFound=warning",
-						  SEARCH_FILTERS,
-						  LAST);
-
 	lr_start_sub_transaction("加载标签属性","相册动态新增商品");
 
 	web_custom_request("get_tags",
-					   "Url=https://{requestUrl}/service/album/album_theme_tag_operation.jsp?act=get_tags&goods_id=&token={userToken}&version=2302",
+					   "Url={requestUrl}/service/album/album_theme_tag_operation.jsp?act=get_tags&goods_id=&token={userToken}&version=2302",
 					   "Method=GET",
 					   "Mode=HTTP",
 						LAST);
 
 	web_custom_request("get_all_tag",
-					   "Url=https://{requestUrl}/service/album/album_group.jsp?act=get_all_tag&type=0&token={userToken}&version=2302",
+					   "Url={requestUrl}/service/album/album_group.jsp?act=get_all_tag&type=0&token={userToken}&version=2302",
 					   "Method=GET",
 					   "Mode=HTTP",
 						LAST);
 
 	lr_end_sub_transaction("加载标签属性",LR_AUTO);
 
+	lr_think_time(3);
+
 	lr_start_sub_transaction("发布商品","相册动态新增商品");
 
 	web_custom_request("save_theme",
-					   "Url=https://{requestUrl}/service/album/album_theme_operation.jsp?act=save_theme&mark_code={mark_code}&client_type=ios&goodsNum=ABC_{randomNum}&main_imgs=%5B%0A%20%20%22https%3A%5C/%5C/xcimg.szwego.com%5C/1547209778_910270265_0%22%2C%0A%20%20%22https%3A%5C/%5C/xcimg.szwego.com%5C/1547209778_257527751_1%22%2C%0A%20%20%22https%3A%5C/%5C/xcimg.szwego.com%5C/1547209778_1237727370_2%22%2C%0A%20%20%22https%3A%5C/%5C/xcimg.szwego.com%5C/1547209778_1374390121_3%22%2C%0A%20%20%22https%3A%5C/%5C/xcimg.szwego.com%5C/1547209778_3772973971_4%22%2C%0A%20%20%22https%3A%5C/%5C/xcimg.szwego.com%5C/1547209778_3229263933_5%22%2C%0A%20%20%22https%3A%5C/%5C/xcimg.szwego.com%5C/1547209778_3646982984_6%22%2C%0A%20%20%22https%3A%5C/%5C/xcimg.szwego.com%5C/1547209778_76362054_7%22%0A%5D&title=Testing_{randomNum}&token={userToken}&version=2302&personal=0&platform=app&share_type=-1&source_type=0&source_url=&sources=&sub_imgs=",
+					   "Url={requestUrl}/service/album/album_theme_operation.jsp?act=save_theme&tagId=&client_type=ios&goodsNum=ABC_{randomNum}&main_imgs={images}&title=Testing_{randomNum}&token={userToken}&version=2302&personal=0&platform=app&share_type=-1&source_type=0",
 					   "Method=POST",
 					   "Mode=HTTP",
 					   LAST);
@@ -52,8 +49,11 @@ Action()
 	
 	lr_start_sub_transaction("返回动态","相册动态新增商品");
 
+	lr_think_time(3);
+
+
 	web_custom_request("owner_1",
-					   "Url=https://{requestUrl}/service/album/get_album_themes_list.jsp?act=owner&shop_id={shop_id}&search_value=&search_img=&start_date=&end_date=&page_index=1&slip_type=0&token={userToken}",
+					   "Url={requestUrl}/service/album/get_album_themes_list.jsp?act=owner&shop_id={shop_id}&search_value=&search_img=&start_date=&end_date=&page_index=1&slip_type=0&token={userToken}",
 					   "Method=POST",
 					   "Mode=HTTP",
 					   LAST);
--- SaveOwner/pre_cci.c
+++ SaveOwner/pre_cci.c
@@ -1,4 +1,4 @@
-# 1 "d:\\performancedata\\saveowner\\\\combined_SaveOwner.c"
+# 1 "d:\\loadrunnerscript\\saveowner\\\\combined_SaveOwner.c"
 # 1 "D:\\LoadRunner11\\include/lrun.h" 1
  
  
@@ -822,7 +822,7 @@ int lr_convert_string_encoding(char *sourceString, char *fromEncoding, char *toE
 
 
 
-# 1 "d:\\performancedata\\saveowner\\\\combined_SaveOwner.c" 2
+# 1 "d:\\loadrunnerscript\\saveowner\\\\combined_SaveOwner.c" 2
 
 # 1 "globals.h" 1
 
@@ -1735,14 +1735,14 @@ int lr_convert_string_encoding(char *sourceString, char *fromEncoding, char *toE
  
 
 
-# 2 "d:\\performancedata\\saveowner\\\\combined_SaveOwner.c" 2
+# 2 "d:\\loadrunnerscript\\saveowner\\\\combined_SaveOwner.c" 2
 
 # 1 "vuser_init.c" 1
 vuser_init()
 {
 	return 0;
 }
-# 3 "d:\\performancedata\\saveowner\\\\combined_SaveOwner.c" 2
+# 3 "d:\\loadrunnerscript\\saveowner\\\\combined_SaveOwner.c" 2
 
 # 1 "Action.c" 1
 Action()
@@ -1750,72 +1750,74 @@ Action()
  
 
 	web_reg_save_param_ex("ParamName=shop_id",
-						"LB=123456\",\"shop_id\":\"",
+						"LB=shop_id\":\"",
 						"RB=\",\"watermark",
 						"NotFound=warning",
 						"SEARCH_FILTERS",
 						"LAST");
+	lr_think_time(3);
 
-	lr_start_transaction("个人相册保存商品");
-	lr_start_sub_transaction("进入相册动态","个人相册保存商品");
+
 	web_custom_request("owner",
-					   "Url=https://{requestUrl}/service/album/get_album_themes_list.jsp?act=owner&shop_id=&search_value=&search_img=&start_date=&end_date=&page_index=1&client_type=ios&token={userToken}&version=2302",
+					   "Url={requestUrl}/service/album/get_album_themes_list.jsp?act=owner&shop_id=&search_value=&search_img=&start_date=&end_date=&page_index=1&client_type=ios&token={userToken}&version=2302",
 					   "Method=POST",
 					   "Mode=HTTP",
 					   "LAST");
-	lr_end_sub_transaction("进入相册动态",2);
 
-	web_reg_save_param_ex("ParamName=mark_code",
-						  "LB=mark_code\":\"",
-						  "RB=\"},\"errmsg",
-						  "NotFound=warning",
-						  "SEARCH_FILTERS",
-						  "LAST");
-	lr_start_sub_transaction("发布商品返回动态","个人相册保存商品");
-	web_custom_request("get_markcode",
-					   "Url=https://{requestUrl}/service/album/album_theme_operation.jsp?act=get_markcode&client_type=ios&platform=app&token={userToken}&version=2302",
+	lr_start_transaction("相册动态新增商品");
+
+	lr_start_sub_transaction("加载标签属性","相册动态新增商品");
+
+	web_custom_request("get_tags",
+					   "Url={requestUrl}/service/album/album_theme_tag_operation.jsp?act=get_tags&goods_id=&token={userToken}&version=2302",
 					   "Method=GET",
 					   "Mode=HTTP",
 						"LAST");
 
-	web_custom_request("save_theme",
-					   "Url=https://{requestUrl}/service/album/album_theme_operation.jsp?act=save_theme&mark_code={mark_code}&client_type=ios&goodsNum=ABC_{randomNum}&main_imgs=%5B%0A%20%20%22https%3A%5C/%5C/xcimg.szwego.com%5C/1547209778_910270265_0%22%2C%0A%20%20%22https%3A%5C/%5C/xcimg.szwego.com%5C/1547209778_257527751_1%22%2C%0A%20%20%22https%3A%5C/%5C/xcimg.szwego.com%5C/1547209778_1237727370_2%22%2C%0A%20%20%22https%3A%5C/%5C/xcimg.szwego.com%5C/1547209778_1374390121_3%22%2C%0A%20%20%22https%3A%5C/%5C/xcimg.szwego.com%5C/1547209778_3772973971_4%22%2C%0A%20%20%22https%3A%5C/%5C/xcimg.szwego.com%5C/1547209778_3229263933_5%22%2C%0A%20%20%22https%3A%5C/%5C/xcimg.szwego.com%5C/1547209778_3646982984_6%22%2C%0A%20%20%22https%3A%5C/%5C/xcimg.szwego.com%5C/1547209778_76362054_7%22%0A%5D&title=Testing_{randomNum}&token={userToken}&version=2302&personal=0&platform=app&share_type=-1&source_type=0&source_url=&sources=&sub_imgs=",
-					   "Method=POST",
+	web_custom_request("get_all_tag",
+					   "Url={requestUrl}/service/album/album_group.jsp?act=get_all_tag&type=0&token={userToken}&version=2302",
+					   "Method=GET",
 					   "Mode=HTTP",
-					   "LAST");
-	lr_end_sub_transaction("发布商品返回动态",2);
-
-	web_custom_request("owner_1",
-					   "Url=https://{requestUrl}/service/album/get_album_themes_list.jsp?act=owner&shop_id={shop_id}&search_value=&search_img=&start_date=&end_date=&page_index=1&slip_type=0&token={userToken}",
-					   "Method=POST",
-					   "Mode=HTTP",
-					   "LAST");
-	
-	lr_start_sub_transaction("发布商品返回个人相册","个人相册保存商品");
-	web_custom_request("save_theme",
-					   "Url=https://{requestUrl}/service/album/album_theme_operation.jsp?act=save_theme&album_id={shop_id}&client_type=ios&goodsNum=ABC_{randomNum}&main_imgs=%5B%0A%20%20%22https%3A%5C/%5C/xcimg.szwego.com%5C/1547209778_910270265_0%22%2C%0A%20%20%22https%3A%5C/%5C/xcimg.szwego.com%5C/1547209778_257527751_1%22%2C%0A%20%20%22https%3A%5C/%5C/xcimg.szwego.com%5C/1547209778_1237727370_2%22%2C%0A%20%20%22https%3A%5C/%5C/xcimg.szwego.com%5C/1547209778_1374390121_3%22%2C%0A%20%20%22https%3A%5C/%5C/xcimg.szwego.com%5C/1547209778_3772973971_4%22%2C%0A%20%20%22https%3A%5C/%5C/xcimg.szwego.com%5C/1547209778_3229263933_5%22%2C%0A%20%20%22https%3A%5C/%5C/xcimg.szwego.com%5C/1547209778_3646982984_6%22%2C%0A%20%20%22https%3A%5C/%5C/xcimg.szwego.com%5C/1547209778_76362054_7%22%0A%5D&title=Testing_{randomNum}&token={userToken}&version=2302",
-					   "Method=POST",
-					   "Mode=HTTP",
-					   "LAST");
-
-	lr_end_sub_transaction("发布商品返回个人相册",2);
-
-	web_custom_request("single_album",
-					   "Url=https://{requestUrl}/service/album/get_album_themes_list.jsp?act=single_album&shop_id={shop_id}&search_value=&search_img=&start_date=&end_date=&tag=[]&page_index=1&from_id=&client_type=ios&token={userToken}&version=2302",
-					   "Method=POST",
-				       "Mode=HTTP",
 						"LAST");
 
-	lr_end_transaction("个人相册保存商品",2);
+	lr_end_sub_transaction("加载标签属性",2);
+
+	lr_think_time(3);
+
+	lr_start_sub_transaction("发布商品","相册动态新增商品");
+
+	web_custom_request("save_theme",
+					   "Url={requestUrl}/service/album/album_theme_operation.jsp?act=save_theme&tagId=%5B%7B%22tagId%22%3A{randomNum}%2C%22tagName%22%3A%22{randomNum}%22%7D%5D&client_type=ios&goodsNum=ABC_{randomNum}&main_imgs={images}&title=Testing_{randomNum}&token={userToken}&version=2302&personal=0&platform=app&share_type=-1&source_type=0",
+					   "Method=POST",
+					   "Mode=HTTP",
+					   "LAST");
+
+	lr_end_sub_transaction("发布商品",2);
+	
+	lr_start_sub_transaction("返回动态","相册动态新增商品");
+
+	lr_think_time(3);
+
+
+	web_custom_request("owner_1",
+					   "Url={requestUrl}/service/album/get_album_themes_list.jsp?act=owner&shop_id={shop_id}&search_value=&search_img=&start_date=&end_date=&page_index=1&slip_type=0&token={userToken}",
+					   "Method=POST",
+					   "Mode=HTTP",
+					   "LAST");
+
+	lr_end_sub_transaction("返回动态",2);
+
+	lr_end_transaction("相册动态新增商品",2);
 
 	return 0;
+	
 }
-# 4 "d:\\performancedata\\saveowner\\\\combined_SaveOwner.c" 2
+# 4 "d:\\loadrunnerscript\\saveowner\\\\combined_SaveOwner.c" 2
 
 # 1 "vuser_end.c" 1
 vuser_end()
 {
 	return 0;
 }
-# 5 "d:\\performancedata\\saveowner\\\\combined_SaveOwner.c" 2
+# 5 "d:\\loadrunnerscript\\saveowner\\\\combined_SaveOwner.c" 2
 
